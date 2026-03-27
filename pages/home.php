1f11cb584d739d655620b8f64d2a978d8e4ba315
9wpvgk26r7-netizen <9wpvgk26r7@privaterelay.appleid.com>
2026-03-27 11:06:06 +0100

    Update 1 home.php

--- pages/home.php
+++ pages/home.php
@@ -1,74 +1,91 @@
 <?php require "includes/header.php" ?>
-    <header>
-        <div class="advertorials">
-            <div class="advertorial">
-                <h2>Hét platform om een auto te huren</h2>
-                <p>Snel en eenvoudig een auto huren. Natuurlijk voor een lage prijs.</p>
-                <a href="#" class="button-primary">Huur nu een auto</a>
-                <img src="assets/images/car-rent-header-image-1.png" alt="">
-                <img src="assets/images/header-circle-background.svg" alt="" class="background-header-element">
+<?php require "database/connection.php" ?>
+ 
+<?php //populair section//
+$query = $conn->prepare("SELECT * FROM autos limit 4");
+$query->execute();
+$popularcar = $query->fetchAll(PDO::FETCH_ASSOC);
+ 
+$query2 = $conn->prepare("SELECT * FROM autos limit 5 OFFSET 4");
+$query2->execute();
+$recommendcar = $query2->fetchAll(PDO::FETCH_ASSOC);
+?>
+ 
+<header>
+    <div class="advertorials">
+        <div class="advertorial">
+            <h2>Hét platform om een auto te huren</h2>
+            <p>Snel en eenvoudig een auto huren. Natuurlijk voor een lage prijs.</p>
+            <a href="#" class="button-primary">Huur nu een auto</a>
+            <img src="assets/images/car-rent-header-image-1.png" alt="">
+            <img src="assets/images/header-circle-background.svg" alt="" class="background-header-element">
+        </div>
+        <div class="advertorial">
+            <h2>Wij verhuren ook bedrijfswagens</h2>
+            <p>Voor een vaste lage prijs met prettig voordelen.</p>
+            <a href="#" class="button-primary">Huur een bedrijfswagen</a>
+            <img src="assets/images/car-rent-header-image-2.png" alt="">
+            <img src="assets/images/header-block-background.svg" alt="" class="background-header-element">
+        </div>
+    </div>
+</header>
+ 
+<main>
+<h2 class="section-title">Populaire auto's</h2>
+<div class="cars">
+ 
+    <?php foreach ($popularcar as $car ) : ?>
+        <div class="car-details">
+            <div class="car-brand">
+                <h3><?= $car['merk']; ?></h3>
+                <div class="car-type"><?= $car['autotype']; ?></div>
             </div>
-            <div class="advertorial">
-                <h2>Wij verhuren ook bedrijfswagens</h2>
-                <p>Voor een vaste lage prijs met prettig voordelen.</p>
-                <a href="#" class="button-primary">Huur een bedrijfswagen</a>
-                <img src="assets/images/car-rent-header-image-2.png" alt="">
-                <img src="assets/images/header-block-background.svg" alt="" class="background-header-element">
-
+ 
+            <img src="assets/images/products/<?= $car['afbeelding']; ?>" alt="">
+ 
+            <div class="car-specification">
+                <span><img src="assets/images/icons/gas-station.svg" alt=""><?= $car['benzine']; ?></span>
+                <span><img src="assets/images/icons/profile-2user.svg" alt=""><?= $car['capaciteit']; ?></span>
+            </div>
+ 
+            <div class="rent-details">
+                <span><span class="font-weight-bold">€<?= $car['prijs']; ?></span> / dag</span>
+                <a href="/car-detail" class="button-primary">Bekijk nu</a>
             </div>
         </div>
-    </header>
-
-    <main>
-    <h2 class="section-title">Populaire auto's</h2>
-    <div class="cars">
-        <?php for ($i = 0; $i <= 3; $i++) : ?>
-            <div class="car-details">
-                <div class="car-brand">
-                    <h3>Koenigegg</h3>
-                    <div class="car-type">
-                        Sport
-                    </div>
-                </div>
-                <img src="assets/images/products/car%20(<?= $i ?>).svg" alt="">
-                <div class="car-specification">
-                    <span><img src="assets/images/icons/gas-station.svg" alt="">90l</span>
-                    <span><img src="assets/images/icons/car.svg" alt="">Schakel</span>
-                    <span><img src="assets/images/icons/profile-2user.svg" alt="">2 Personen</span>
-                </div>
-                <div class="rent-details">
-                    <span><span class="font-weight-bold">€249,00</span> / dag</span>
-                    <a href="/car-detail" class="button-primary">Bekijk nu</a>
-                </div>
-            </div>
-        <?php endfor; ?>
+    <?php endforeach; ?>
+ 
+</div>
+ 
+<h2 class="section-title">Aanbevolen auto's</h2>
+<div class="cars">
+ 
+<?php foreach ($recommendcar as $car) : ?>
+    <div class="car-details">
+        <div class="car-brand">
+            <h3><?= $car['merk']; ?></h3>
+            <div class="car-type"><?= $car['autotype']; ?></div>
+        </div>
+ 
+        <img src="assets/images/products/<?= $car['afbeelding']; ?>" alt="">
+ 
+        <div class="car-specification">
+                <span><img src="assets/images/icons/gas-station.svg" alt=""><?= $car['brandstof']; ?></span>
+                <span><img src="assets/images/icons/profile-2user.svg" alt=""><?= $car['capaciteit']; ?></span>
+        </div>
+ 
+        <div class="rent-details">
+            <span><span class="font-weight-bold">€<?= $car['prijs']; ?></span> / dag</span>
+            <a href="/car-detail" class="button-primary">Bekijk nu</a>
+        </div>
     </div>
-    <h2 class="section-title">Aanbevolen auto's</h2>
-    <div class="cars">
-        <?php for ($i = 4; $i <= 11; $i++) : ?>
-            <div class="car-details">
-                <div class="car-brand">
-                    <h3>Koenigegg</h3>
-                    <div class="car-type">
-                        Sport
-                    </div>
-                </div>
-                <img src="assets/images/products/car%20(<?= $i ?>).svg" alt="">
-                <div class="car-specification">
-                    <span><img src="assets/images/icons/gas-station.svg" alt="">90l</span>
-                    <span><img src="assets/images/icons/car.svg" alt="">Schakel</span>
-                    <span><img src="assets/images/icons/profile-2user.svg" alt="">2 People</span>
-                </div>
-                <div class="rent-details">
-                    <span><span class="font-weight-bold">€249,00</span> / dag</span>
-                    <a href="/car-detail" class="button-primary">Bekijk nu</a>
-                </div>
-            </div>
-        <?php endfor; ?>
-    </div>
-    <div class="show-more">
-        <a class="button-primary" href="#">Toon alle</a>
-    </div>
-    </main>
-
+<?php endforeach; ?>
+ 
+</div>
+ 
+<div class="show-more">
+    <a class="button-primary" href="#">Toon alle</a>
+</div>
+</main>
+ 
 <?php require "includes/footer.php" ?>
