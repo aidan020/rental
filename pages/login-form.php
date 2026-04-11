fb76b055c653d4d45238085360077062b4fddecc
aidan020 <9029086@student.zadkine.nl>
2026-04-12 00:29:20 +0200

    Refactor login form for better session message handling

--- pages/login-form.php
+++ pages/login-form.php
@@ -3,15 +3,27 @@
 <main>
     <form action="/login-handler" class="account-form" method="post">
         <h2>Log in</h2>
-        <?php if (isset($_SESSION['success'])) { ?>
-            <div class="succes-message"><?= $_SESSION['success'] ?></div>
-        <?php } ?>
+
+        <?php if (isset($_SESSION['message'])): ?>
+            <div class="message"><?= htmlspecialchars($_SESSION['message']) ?></div>
+            <?php unset($_SESSION['message']); ?>
+        <?php endif; ?>
+
+        <?php if (isset($_SESSION['success'])): ?>
+            <div class="succes-message"><?= htmlspecialchars($_SESSION['success']) ?></div>
+            <?php unset($_SESSION['success']); ?>
+        <?php endif; ?>
+
         <label for="Email">Uw e-mail</label>
-        <input type="Email" name="Email" id="Email" placeholder="johndoe@gmail.com" value="<?= isset($_SESSION['Email']) ? htmlspecialchars($_SESSION['Email']) : '' ?>" required autofocus>
+        <input type="email" name="Email" id="Email" placeholder="johndoe@gmail.com" required autofocus>
+
         <label for="password">Uw wachtwoord</label>
-    <input type="password" name="password" d="password" placeholder="Uw wachtwoord" required>
+        <input type="password" name="password" id="password" placeholder="Uw wachtwoord" required>
+
         <input type="submit" value="Log in" class="button-primary">
+
+        <p>Nog geen account? <a href="/register-form">Registreren</a></p>
     </form>
 </main>
- 
+
 <?php require "includes/footer.php" ?>
