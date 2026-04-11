b7f24bc12a705c3f548ec541eacfc0ab7082b74c
aidan020 <9029086@student.zadkine.nl>
2026-04-12 00:32:17 +0200

    Update rental.sql

--- rental.sql
+++ rental.sql
@@ -1,18 +1,18 @@
 
 CREATE TABLE `account` (
-  `id` int NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `Id` int NOT NULL,
+  `Email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` int DEFAULT NULL
 );
 
-INSERT INTO `account` (`id`, `email`, `password`, `role`) VALUES
+INSERT INTO `account` (`Id`, `Email`, `password`, `role`) VALUES
 (9, 'kelvin@kelvin.nl', '$2y$12$w2fuXiPg1m2jC.C9BCCB5ebeEPNUcwxVp2StqdFJa9y62xwwmfKWK', NULL),
 (10, 'cassandra@cassandra.nl', '$2y$12$pVGqaOKe9t0QZZozeub4ueghtgx09JEKWb/ohSPhh6VCucC8Zpplm', NULL);
 
 ALTER TABLE `account`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Email` (`Email`);
   
 ALTER TABLE `account`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
